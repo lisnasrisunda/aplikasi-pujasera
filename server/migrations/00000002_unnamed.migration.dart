ee80ed6dff13f0b06e12433ac194463d7f47d723
lisnawati-sr <70515909+lisnawati-sr@users.noreply.github.com>
2020-09-01 04:25:46 +0700

    first commit

--- server/migrations/00000002_unnamed.migration.dart
+++ server/migrations/00000002_unnamed.migration.dart
@@ -7,7 +7,6 @@ class Migration2 extends Migration {
    		database.createTable(SchemaTable("PENGGUNA", [SchemaColumn("username", ManagedPropertyType.string, isPrimaryKey: true, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("password", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("role", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false)]));
 		database.createTable(SchemaTable("TENAN", [SchemaColumn("id", ManagedPropertyType.integer, isPrimaryKey: true, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("nama", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("alamat", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),SchemaColumn("noTelp", ManagedPropertyType.string, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false)]));
 		database.addColumn("TENAN", SchemaColumn.relationship("pengguna", ManagedPropertyType.string, relatedTableName: "PENGGUNA", relatedColumnName: "username", rule: DeleteRule.nullify, isNullable: false, isUnique: false));
-		// database.deleteTable("USER");
   }
   
   @override
